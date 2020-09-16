3e5af4572097a047298694ad3c05d36f5926a1aa
silk0 <46167286+silk0@users.noreply.github.com>
2020-09-15 22:33:54 -0600

    Cambio Recien Hecho

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using Financiero.Modelo;
-using System;
-using System.Net.Configuration;
-using System.Net.Mail;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Financiero
@@ -18,14 +15,6 @@ namespace Financiero
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new InicioSesion());
-
-            //Este modificacion
-
-            /*EmailSenderOptions option = new EmailSenderOptions();
-            EmailSender emailSender = new EmailSender(option);
-            emailSender.SendEmailAsync("silkfirmyn@gmail.com","Envio de token");
-            MessageBox.Show("Correo Enviado correctamente");*/
-                   
         }
     }
 }
